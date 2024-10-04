821444b34560397d55fe3fc61467c96c961a1d9c
Aashna Joshi <aashnahetal@gmail.com>
2024-10-04 16:54:28 +0530

    .

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
 import pandas as pd
 
-# Activate pandas-to-R conversion
 pandas2ri.activate()
-
-# Import R's ggplot2 package
 ggplot2 = importr('ggplot2')
 
 # Run R code
@@ -17,7 +14,7 @@ plot <- ggplot(mtcars, aes(x=wt, y=mpg)) + geom_point()
 ggsave(filename="plot.png", plot=plot)
 ''')
 
-# Load the plot into Python if needed
-from PIL import Image
+# Load the plot into Python if need
+'''from PIL import Image
 img = Image.open("plot.png")
-img.show()
+img.show()'''
